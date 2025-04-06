84a5068225dc7952843e5c05bb2af2cd5558fc53
SaHair529 <sa.hairulaev@gmail.com>
2025-04-07 00:43:39 +0300

    Добавил ручку для создания статусов

--- backend/src/Controller/StatusController.php
+++ backend/src/Controller/StatusController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Status;
+use App\Enum\ErrorCode;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -22,5 +24,46 @@ class StatusController extends AbstractController
         $statuses = $this->em->getRepository(Status::class)->findAll();
 
         return $this->json($statuses, Response::HTTP_OK, [], ['groups' => 'status_compact']);
+    }
+
+    #[Route('/', methods: ['POST'])]
+    public function create(Request $request): JsonResponse
+    {
+        $requestData = json_decode($request->getContent(), true);
+
+        if (
+            !isset($requestData['name'], $requestData['color'], $requestData['index']) ||
+            !$requestData['name'] || !$requestData['color'] || !$requestData['index']
+        )
+        {
+            return $this->json([
+                'error_message' => 'Invalid request data',
+                'error_code' => ErrorCode::INVALID_REQUEST_DATA->value
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        $statuses =  $this->em->getRepository(Status::class)
+            ->createQueryBuilder('s')
+            ->where('s.index >= :startIndex')
+            ->setParameter('startIndex', $requestData['index'])
+            ->orderBy('s.index', 'ASC') // Сортировка по возрастанию
+            ->getQuery()
+            ->getResult();
+
+        foreach ($statuses as $status) {
+            $status->setIndex($status->getIndex()+1);
+            $this->em->persist($status);
+        }
+        $this->em->flush();
+
+        $status = new Status();
+        $status->setName($requestData['name'])
+            ->setColor($requestData['color'])
+            ->setIndex($requestData['index']);
+
+        $this->em->persist($status);
+        $this->em->flush();
+
+        return $this->json(null, Response::HTTP_CREATED);
     }
 }
